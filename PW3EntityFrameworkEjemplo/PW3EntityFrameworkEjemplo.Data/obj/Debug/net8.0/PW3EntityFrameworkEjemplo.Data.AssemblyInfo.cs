38edc341d29cfd2665234f594aa2f06735b21397
AlanAruFlores <alanaruquipa242@gmail.com>
2024-10-18 14:03:37 -0300

    entity

--- PW3EntityFrameworkEjemplo/PW3EntityFrameworkEjemplo.Data/obj/Debug/net8.0/PW3EntityFrameworkEjemplo.Data.AssemblyInfo.cs
+++ PW3EntityFrameworkEjemplo/PW3EntityFrameworkEjemplo.Data/obj/Debug/net8.0/PW3EntityFrameworkEjemplo.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PW3EntityFrameworkEjemplo.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579496d1b42dc705c3eca240487ab81ba899c540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ab58923ec40cc0ac8c053cf201e5d14cdaeccd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PW3EntityFrameworkEjemplo.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PW3EntityFrameworkEjemplo.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
